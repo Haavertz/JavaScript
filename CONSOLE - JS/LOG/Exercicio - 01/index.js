444a7513486153c7f2d14442b5ac53dcde3982d5
Gleison <gleisonsenac06@gmail.com>
2023-12-04 01:03:05 -0300

    Initial Commit
    subindo 2 exercicios de concatenacao e etc

---
+++ CONSOLE - JS/LOG/Exercicio - 01/index.js
@@ -0,0 +1,2 @@
+// Normalmente utilizado para aparecer algo na tela .log
+console.log("Hello, World!");
---
+++ CONSOLE - JS/LOG/Exercicio - 02/index.js
@@ -0,0 +1,21 @@
+// Mostrar meu nome e alguns outros dados
+
+console.log("Gleison Morais da SIlva");
+console.log("09/06/2006");
+console.log(2006);
+
+console.log("-=-=-=-=-=-=-=-=-");
+
+// Mostrando que se pode passar varios argumentos para o log
+console.log("Gleison Morais (2006)");
+console.log("Gleison", " Morais ", "(2006)");
+
+console.log("-=-=-=-=-=-=-=-=-");
+
+console.log("Gleison")
+console.log("Morais")
+console.log("(2006)")
+
+console.log("-=-=-=-=-=-=-=-=-");
+
+console.log("Gleison" + "Morais" + "da Silva")
