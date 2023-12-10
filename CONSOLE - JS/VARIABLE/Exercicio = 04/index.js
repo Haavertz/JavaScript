e9196d634505d91402aa9c76d87c7bb4f2482644
Gleison <gleisonsenac06@gmail.com>
2023-12-10 18:21:36 -0300

    Index - 04
    fazendo algumas coisas com o console.log e variaveis.

---
+++ CONSOLE - JS/VARIABLE/Exercicio = 04/index.js
@@ -0,0 +1,23 @@
+let steps = 100;
+console.log(steps); // -> 100
+steps = 120; // -> 120
+console.log(steps);
+steps = steps + 200;
+console.log(steps); // -> 320
+
+let greeting = "Hello!";
+let counter = 100;
+
+console.log(greeting); // -> Hello!
+greeting = 1;
+console.log(greeting); // -> 1
+
+greeting = "Hello!";
+greeting = greeting + counter;
+console.log(greeting); // -> Hello!100
+
+// -> Concatenacão
+
+const counter2 = 1;
+greeting = greeting + counter2;
+console.log(greeting); // -> Hello!100
