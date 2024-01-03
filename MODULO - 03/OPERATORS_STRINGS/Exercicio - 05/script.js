d1fad00d6d49f4b315f12acbd858a584d0d5fb4c
Gleison <gleisonsenac06@gmail.com>
2024-01-03 20:30:24 -0300

    Update Finaly Script Strings

---
+++ MODULO - 03/OPERATORS_STRINGS/Exercicio - 05/script.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = a + 2 * 3;
+let c = a + 2  < 20 - 15;
+console.log(a); // -> 10
+console.log(b); // -> 16
+console.log(c); // -> false
+
+let e, d;
+d = (e = (20 + 20) * 2) > (3 ** 2);
+console.log(e); // -> 80
+console.log(d); // -> true
