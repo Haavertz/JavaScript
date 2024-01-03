3470caa9aef4645d5d218d90e97baf2002fd0a67
Gleison <gleisonsenac06@gmail.com>
2024-01-03 20:10:04 -0300

    Update Operators

---
+++ MODULO - 03/ASSIGNMENT_OPERATORS/Exercicio - 01/script.js
@@ -0,0 +1,24 @@
+const name = "Alice";
+console.log(name); // -> Alice
+
+// let year = 2050;
+// let newYear = year = 2051;
+
+let year = 2050;
+year = 2051;
+let newYear = year;
+
+
+console.log(2 + 2 * 2); // -> 6
+console.log(2 + (2 * 2)); // -> 6
+console.log((2 + 2) * 2); // -> 8
+
+const x = 5;
+const y = 2;
+
+console.log("addition: ", x + y); // -> 7
+console.log("subtraction: ", x - y); // -> 3
+console.log("multiplication: ", x * y); // -> 10
+console.log("division: ", x / y); // -> 2.5
+console.log("division remainder :", x % y); // -> 1
+console.log("exponent: ", x ** y); // -> 25
