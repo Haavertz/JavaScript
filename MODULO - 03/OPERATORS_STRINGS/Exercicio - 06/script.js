2e9eb7286081b8ace6cdb6f3035241f6ea685fe9
Gleison <gleisonsenac06@gmail.com>
2024-01-09 00:08:44 -0300

    Update Exercicio 06 e create script 07

---
+++ MODULO - 03/OPERATORS_STRINGS/Exercicio - 06/script.js
@@ -0,0 +1,30 @@
+let isUserReady = confirm("Are you ready?");
+console.log(isUserReady);
+if (isUserReady) {
+    alert("User ready!");
+}
+
+let unitPrice = 10;
+let pieces = prompt("How many pieces do you order?", 0);
+
+if (pieces > 0) {
+    let total = unitPrice * pieces;
+    console.log(total);
+}
+
+console.log(total); // -> Uncaught ReferenceError: total is not defined
+
+
+let userAge = 23;
+let isFemale = false;
+let points = 703;
+let cartValue = 299;
+let shippingCost = 9.99;
+
+if (userAge > 21) {
+    if (cartValue >= 300 || points >= 500) {
+        shippingCost = 0;
+    }
+}
+
+console.log(shippingCost);
